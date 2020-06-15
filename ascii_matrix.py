ca06a09285e515640207d3c152890518fc59d419
kevinelong <kevinelong@gmail.com>
2020-06-15 01:37:01 -0700

    Initial commit

---
+++ ascii_matrix.py
@@ -0,0 +1,56 @@
+class ASCII():
+
+    def __init__(self):
+        self.columns = 8
+        self.limit = 128
+
+    def print_matrix(self):
+        print("\n".join( [
+            self.title(),
+            self.get_columns(),
+            self.row_two(),
+            self.body(), ]))
+
+    def title(self):
+        return "ASCII Matrix:"
+
+
+    def get_columns(self):
+        output = ["DECIMAL", ""]
+        for column in range(0,self.columns):
+            output.append(" {} ".format(column))
+        return "\t".join(output)
+
+    def row_two(self):
+        output=["", "BINARY"]
+        for column in range(0, self.columns):
+            output.append("{0:03b}".format(column))
+        return "\t".join(output)
+
+    def body(self):
+        output = []
+        for high_nibble in range(0, self.limit // self.columns):
+            output.append(self.row(high_nibble))
+        return "\n".join(output)
+
+    def row(self, high_nibble):
+        r = high_nibble * self.columns
+        output = []
+        output.append(" {0:03d}".format(r))
+        output.append("{0:04b}".format(high_nibble))
+
+        for low_nibble in range(0, self.columns):
+            output.append(self.column(r, low_nibble))
+        return "\t".join(output)
+
+    def column(self, r, low_nibble):
+        v = r + low_nibble
+        if v <= 27:
+            return "^{}={}".format(chr(v+64),v)
+        else:
+            return "'{}'={}".format(chr(v),v)
+
+
+if __name__ == "__main__":
+    ASCII().print_matrix()
+
